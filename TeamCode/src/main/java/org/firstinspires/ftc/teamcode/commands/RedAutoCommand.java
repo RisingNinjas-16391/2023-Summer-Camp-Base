c6c9e2f67a2ca34c79c42003a547620e927ce932
A <local@MacBookAir-M1-AHS789.local>
2023-07-12 15:36:21 -0400

    wednesday auto work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/RedAutoCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/RedAutoCommand.java
@@ -13,10 +13,21 @@ public class RedAutoCommand extends SequentialCommandGroup {
 
     public RedAutoCommand(MecanumDrive drive, PivotSubsystem pivot, FeederSubsystem feeder) {
         TrajectorySequenceSupplier pathA = () -> drive.trajectorySequenceBuilder((new Pose2d()))
-                .forward(-10)
-                .back(-10)
-                .strafeRight(-10)
-                .strafeLeft(-10)
+                .forward(12)
+                /// arm down, sushi wheels spin, arm up
+                .back(13)
+                .turn(Math.toRadians(180))
+                .forward(65.5)
+                /// arm down, sushi wheels spin, arm up
+                .back(13)
+                .turn(Math.toRadians(180))
+                .forward(65.5)
+                /// place cone
+                .back(78.5)
+                .strafeLeft(49)
+                .forward(63)
+                /// arm down, sushi wheels spin, arm up
+
                 .build();
 
         SequentialCommandGroup autoRed = new SequentialCommandGroup(
